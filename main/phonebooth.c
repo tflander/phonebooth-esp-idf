643f5fb13362fabf065f7fcb7030bb5bc69e73d8
Raymond Willett <raymond.s.willett@accenture.com>
2019-08-26 15:23:05 -0400

    Trying to get the onboard LED to blink

--- main/phonebooth.c
+++ main/phonebooth.c
@@ -4,7 +4,22 @@
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 
-#define BLINK_GPIO CONFIG_BLINK_GPIO
+// #define BLINK_GPIO 9
+// CONFIG_BLINK_GPIO
+
+void do_blink(int BLINK_GPIO) {
+    gpio_pad_select_gpio(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    while(1) {
+        /* Blink on (output high) */
+        gpio_set_level(BLINK_GPIO, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        /* Blink off (output low) */
+        gpio_set_level(BLINK_GPIO, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
 
 void blink_task(void *pvParameter)
 {
@@ -14,17 +29,13 @@ void blink_task(void *pvParameter)
        Technical Reference for a list of pads and their default
        functions.)
     */
-    gpio_pad_select_gpio(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-    while(1) {
-        /* Blink off (output low) */
-        gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        /* Blink on (output high) */
-        gpio_set_level(BLINK_GPIO, 1);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
+   const int MAX_PIN = 32;
+   for(;;) {
+        for (int i = 0; i < MAX_PIN; i++) {
+            do_blink(i);
+            printf("Pin %d\n", i);
+        }
+   }
 }
 
 void app_main()
