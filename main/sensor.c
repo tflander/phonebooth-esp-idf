6ef60578e9d44064deb3ed3f13f1d0769f397465
Clay Dowling <clayton.c.dowling@accenture.com>
2019-08-27 16:20:31 -0400

    #3 make test pass

--- main/sensor.c
+++ main/sensor.c
@@ -1,8 +1,12 @@
 #include <stdbool.h>
 
 #include "sensor.h"
+#include <driver/gpio.h>
 
 bool initialize_sensor(gpio_num_t pin)
 {
+    gpio_config_t io_conf;
+    io_conf.pin_bit_mask = 1ULL << GPIO_NUM_4;
+    gpio_config(&io_conf);
     return false;
 }
