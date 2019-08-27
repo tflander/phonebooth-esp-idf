1bfbfd21c1bcc4c0741875ad7ddf80b05e5ee073
Clay Dowling <clayton.c.dowling@accenture.com>
2019-08-27 16:45:46 -0400

    #3 First passintest

--- main/sensor.c
+++ main/sensor.c
@@ -3,10 +3,12 @@
 #include "sensor.h"
 #include <driver/gpio.h>
 
+static gpio_config_t sensor_conf;
+
 bool initialize_sensor(gpio_num_t pin)
 {
-    gpio_config_t io_conf;
-    io_conf.pin_bit_mask = 1ULL << GPIO_NUM_4;
-    gpio_config(&io_conf);
+    sensor_conf.pin_bit_mask = 1ULL << pin;
+    sensor_conf.intr_type = GPIO_INTR_NEGEDGE;
+    gpio_config(&sensor_conf);
     return false;
 }
--- test/test_sensor.c
+++ test/test_sensor.c
@@ -21,7 +21,8 @@ TEST(Sensor, initializeSensor_InitializesSensorPin)
     initialize_sensor(PIN);
     const gpio_config_t *g = gpio_config_called_with();
     TEST_ASSERT_NOT_NULL(g);
-    TEST_ASSERT_EQUAL_INT64(g->pin_bit_mask, 1ULL << GPIO_NUM_4);
+    TEST_ASSERT_EQUAL_INT(GPIO_INTR_NEGEDGE, g->intr_type);
+    TEST_ASSERT_EQUAL_INT64(1ULL << PIN, g->pin_bit_mask);
 }
 
 TEST_GROUP_RUNNER(Sensor)
