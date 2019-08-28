f383c9c3a7f810090e195ba51d5694d6d45c00e4
Clay Dowling <clayton.c.dowling@accenture.com>
2019-08-28 10:33:03 -0400

    Added new asserts for #3

--- test/test_sensor.c
+++ test/test_sensor.c
@@ -23,6 +23,11 @@ TEST(Sensor, initializeSensor_InitializesSensorPin)
     TEST_ASSERT_NOT_NULL(g);
     TEST_ASSERT_EQUAL_INT(GPIO_INTR_NEGEDGE, g->intr_type);
     TEST_ASSERT_EQUAL_INT64(1ULL << PIN, g->pin_bit_mask);
+
+    // TODO Pass the following assertions
+    TEST_ASSERT_EQUAL_INT(GPIO_MODE_INPUT, g->mode);
+    TEST_ASSERT_EQUAL_INT(GPIO_PULLUP_DISABLE, g->pull_up_en);
+    TEST_ASSERT_EQUAL_INT(GPIO_PULLDOWN_ENABLE, g->pull_down_en);
 }
 
 TEST_GROUP_RUNNER(Sensor)
