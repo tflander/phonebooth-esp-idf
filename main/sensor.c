3e9621610f9ce6aa13f7a2d4bd5b1c20845b4a04
Raymond Willett <raymond.s.willett@accenture.com>
2019-08-28 10:36:30 -0400

    assertions for gpio mode, pullup, and pulldown

--- main/sensor.c
+++ main/sensor.c
@@ -9,6 +9,9 @@ bool initialize_sensor(gpio_num_t pin)
 {
     sensor_conf.pin_bit_mask = 1ULL << pin;
     sensor_conf.intr_type = GPIO_INTR_NEGEDGE;
+    sensor_conf.mode = GPIO_MODE_INPUT;
+    sensor_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    sensor_conf.pull_down_en = GPIO_PULLDOWN_ENABLE;
     gpio_config(&sensor_conf);
     return false;
 }
