18cdd0e2afdba1a55e79f26220007a31621322ae
Kevin <5399649+kwei774@users.noreply.github.com>
2019-09-03 15:59:22 -0400

    #13 stripped out important bits of gpio

--- mock/driver/gpio.h
+++ mock/driver/gpio.h
@@ -120,21 +120,5 @@ typedef int32_t esp_err_t;
 
 typedef void (*gpio_isr_t)(void *);
 
-esp_err_t gpio_set_direction(gpio_num_t gpio_num, gpio_mode_t mode);
-
-esp_err_t gpio_set_level(gpio_num_t gpio_num, uint32_t level);
-
-esp_err_t gpio_config(const gpio_config_t *pGPIOConfig);
-
-void gpio_pad_select_gpio(uint8_t gpio_num);
-
-esp_err_t gpio_isr_handler_add(gpio_num_t gpio_num, gpio_isr_t isr_handler,
-                               void *args);
-
-int gpio_get_level(gpio_num_t gpio_num);
-
-esp_err_t gpio_set_intr_type(gpio_num_t gpio_num, gpio_int_type_t intr_type);
-
-esp_err_t gpio_install_isr_service(int intr_alloc_flags);
 
 #endif
--- mock/mocktime.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MOCKTIME_H_
-#define _MOCKTIME_H_
-
-#include "newlib/time.h"
-
-useconds_t usleep_was_called_with();
-
-#endif
