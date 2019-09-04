7a7b0e14b39c1259572320edad1fd7dee6879169
Clay Dowling <clayton.c.dowling@accenture.com>
2019-09-04 13:25:38 -0400

    #11 implement passing implementation for led test case

--- main/led.c
+++ main/led.c
@@ -1,5 +1,10 @@
 #include "led.h"
+#include "driver/gpio.h"
 
 void initialize_led(int pinNumber) {
-  // Do Nothing
+  gpio_config_t c;
+  c.pin_bit_mask = 1ULL << pinNumber;
+  c.mode = GPIO_MODE_OUTPUT;
+  c.intr_type = GPIO_INTR_DISABLE;
+  gpio_config(&c);
 }
